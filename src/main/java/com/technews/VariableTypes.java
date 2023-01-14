eda98aa64a511bdaff6e80cba684288ac404659c
Nicholas Defex <ndefected15@gmail.com>
2023-01-14 12:47:08 -0500

    completed leson 2

---
+++ src/main/java/com/technews/VariableTypes.java
@@ -0,0 +1,19 @@
+package com.technews;
+
+public class VariableTypes {
+    public static void main(String[] args) {
+//        The String keyword indicates a string of characters, as shown in the following example:
+        String name = "Cleopatra";
+
+//        The int keyword indicates whole numbers (or integers, like 1, 2, or 3), as follows:
+        int age = 2016 + 69;
+
+//        The double data type stores decimal values (like 1.2, 2.3, or 3.54), as shown in the following example: The double data type stores decimal values (like 1.2, 2.3, or 3.54), as shown in the following example:
+        double price = 1999.99;
+
+//        A boolean data type stores Boolean values (either true or false), as shown in the following example:
+        boolean truth = true;
+        boolean falseness = !truth;
+
+    }
+}
---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,18 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args){
+        int a = 2;
+        double b = 0.5;
+        String c = "cat";
+         // calculated variable sum is a double
+        double sum = a + b;
+        // calculated variable concat is a String
+        String concat = a + c;
+        // value of sum is 7.5
+        System.out.println(sum);
+        // value of concat is "4cat"
+        System.out.println(concat);
+
+    }
+}
